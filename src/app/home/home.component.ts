2be25158f2b3704a627ee161d7fa90c8027cc637
Juan Fra <juanfrancisco.martinez@ficosa.com>
2022-03-02 22:49:29 +0100

    Test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { DataService } from '../data.service';
+import { ICoins } from './i-coins';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  coins: ICoins[] = [];
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit(): void {
+    this.populateCoins();
+  }
+
+  private populateCoins() {
+    this.dataService.getData().subscribe((event: HttpEvent<any>) => {
+      switch (event.type) {
+        case HttpEventType.Sent:
+          console.log('Request sent!');
+          break;
+        case HttpEventType.ResponseHeader:
+          console.log('Response header received!');
+          break;
+        case HttpEventType.DownloadProgress:
+          const kbLoaded = Math.round(event.loaded / 1024);
+          console.log(`Download in progress! ${kbLoaded}Kb loaded`);
+          break;
+        case HttpEventType.Response:
+          console.log('Done!', event.body);
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(event.body, 'text/html');
+          const price_el = doc.getElementsByClassName('td-price');
+          const item_el = doc.getElementsByClassName('tw-w-12');
+          let counter = 1
+          for (let i = 0; i <= 99; i++) {
+            this.coins.push({
+              item: item_el[i].textContent?.trim(),
+              price: price_el[i+counter].textContent?.trim()
+            })
+            counter++
+          }
+      }
+    });
   }
 
 }
---
+++ src/app/home/i-coins.ts
@@ -0,0 +1,4 @@
+export interface ICoins {
+  item: string | undefined;
+  price: string | undefined;
+}
