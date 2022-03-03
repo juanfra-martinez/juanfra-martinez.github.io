a33fa177532a49a778298e4c5ad87020f1943a3d
juanfra-martinez <es.juanfra.martinez@gmail.com>
2022-03-03 14:50:01 +0100

    Crypto table added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 import { ICoins } from './i-coins';
 
 @Component({
@@ -13,25 +14,26 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   async ngOnInit() {
-    const content = await this.http
-      .get<any>('https://jfmartinez-api.vercel.app/prices')
-      .toPromise();
+    const content = await firstValueFrom(
+      this.http.get<any>('https://jfmartinez-api.vercel.app/prices')
+    );
+
     if (!content) {
       return;
     }
     const parser = new DOMParser();
-    const doc = parser.parseFromString(content, 'text/html');
+    const doc = parser.parseFromString(content.body, 'text/html');
     const price_el = doc.getElementsByClassName('td-price');
     const item_el = doc.getElementsByClassName('tw-w-12');
-    const results = [];
+    this.coins = [];
     let counter = 1;
     for (let i = 0; i <= 99; i++) {
-      results.push({
+      this.coins.push({
         item: item_el[i].textContent?.trim(),
         price: price_el[i + counter].textContent?.trim(),
       });
       counter++;
     }
-    console.log(results);
+    console.log(this.coins);
   }
 }
